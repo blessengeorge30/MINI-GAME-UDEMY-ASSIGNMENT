23323b1378468e70c5c45abe1202f99d12566415
Blessen George <blessen.george30@gmail.com>
2024-10-25 15:03:08 +0530

    first commit

---
+++ components/PrimaryButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+function PrimaryButton() {
+  return (
+    <View>
+      <Text>PrimaryButtton</Text>
+    </View>
+  )
+}
+
+export default PrimaryButton
+
+const styles = StyleSheet.create({})
